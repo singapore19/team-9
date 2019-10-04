a14929d63182cf36d86ef0b941648cf0ba1ea109
lch <abc@gmail.com>
2019-10-04 14:10:06 +0800

    add userform

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login.vue'
 import AdminHome from './views/AdminHome'
+import UserForm from './views/UserForm'
 
 Vue.use(Router)
 
@@ -17,5 +18,10 @@ export default new Router({
         name: 'admin-home',
         component: AdminHome
       },
+    {
+      path: '/userform',
+      name: 'userform',
+      component: UserForm
+    }
   ]
 })
---
+++ front-end/src/views/UserForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-layout row class="text-xs-center">
+      <v-flex xs4 class="grey lighten-4">
+        <v-container style="position: relative;top: 13%;" class="text-xs-center">
+          <v-img src="../assets/main-logo.png"></v-img>
+          <v-card flat>
+            <v-card-title primary-title>
+              <h4>Login</h4>
+            </v-card-title>
+            <v-form @submit.prevent="formSubmit" id="user-form">
+              <v-text-field  name="Address" label="Address" v-model="address"></v-text-field>
+              <v-text-field  name="Password" label="Password" type="password" v-model="password"></v-text-field>
+              <v-select
+                      :items="items"
+                      label="Role"
+                      dense
+              ></v-select>
+              <v-card-actions>
+                <v-btn primary large block type="submit" color="success" form="user-form">Login</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>

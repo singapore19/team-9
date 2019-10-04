b14576948227c8363e7d58d0ba755e5c35054c0d
LiMengyang <lime0040@e.ntu.edu.sg>
2019-10-04 15:46:48 +0800

    Added admin

--- front-end/src/views/AdminHome.vue
+++ front-end/src/views/AdminHome.vue
@@ -1,162 +1,168 @@
 <template>
   <v-container>
-    <v-dialog v-model="dialog" scrollable max-width="300px">
-      <v-card>
-        <v-card-title>Reschedule Time</v-card-title>
-        <v-divider></v-divider>
-        <v-card-text style="height: 300px;">
-          <v-form ref="form" v-model="valid" :lazy-validation="lazy">
-            <typography class="display-5">Select Date</typography>
-            <v-date-picker v-model="date" width="200px"></v-date-picker>
-            <br />
-            <typography class="display-5">Select Time</typography>
-            <v-time-picker v-model="time" width="200px"></v-time-picker>
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false; ifChanged = true">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-layout row class="text-xs-center">
+      <v-flex xs4 class="grey lighten-4">
+        <v-container style="position: relative" class="text-xs-center">
+          <v-dialog v-model="dialog" scrollable max-width="300px">
+            <v-card>
+              <v-card-title>Reschedule Time</v-card-title>
+              <v-divider></v-divider>
+              <v-card-text style="height: 300px;">
+                <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+                  <typography class="display-5">Select Date</typography>
+                  <v-date-picker v-model="date" width="200px"></v-date-picker>
+                  <br />
+                  <typography class="display-5">Select Time</typography>
+                  <v-time-picker v-model="time" width="200px"></v-time-picker>
+                </v-form>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+                <v-btn color="blue darken-1" text @click="dialog = false; ifChanged = true">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
 
-    <v-row class="mb-6" gutters="20">
-      <v-col cols="3" justify="top">
-        <typography>10.5</typography>
-      </v-col>
-      <v-col cols="17">
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Buona Vista</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text v-if="!ifChanged">Time: {{tempTime}}</v-card-text>
-          <v-card-text v-else>Time: 16:00</v-card-text>
-          <v-card-actions>
-            <v-btn text @click="dialog = true">Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+          <v-row class="mb-6" gutters="20">
+            <v-col cols="3" justify="top">
+              <typography>Date: 10.5</typography>
+            </v-col>
+            <v-col cols="17">
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Buona Vista</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text v-if="!ifChanged">Time: {{tempTime}}</v-card-text>
+                <v-card-text v-else>Time: 16:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text @click="dialog = true">Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto" style="padding-top:5px">
-          <v-card-title>Address: Pioneer</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+              <v-card max-width="344" class="mx-auto" style="padding-top:5px">
+                <v-card-title>Address: Pioneer</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Laboardo Park</v-card-title>
-          <v-card-text>Type: Donation</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Laboardo Park</v-card-title>
+                <v-card-text>Type: Donation</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
 
-    <v-row class="mb-6" gutters="20">
-      <v-col cols="3" justify="top">
-        <typography>10.6</typography>
-      </v-col>
-      <v-col cols="17">
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Buona Vista</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+          <v-row class="mb-6" gutters="20">
+            <v-col cols="3" justify="top">
+              <typography>Date: 10.6</typography>
+            </v-col>
+            <v-col cols="17">
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Buona Vista</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Pioneer</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Pioneer</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Laboardo Park</v-card-title>
-          <v-card-text>Type: Donation</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Laboardo Park</v-card-title>
+                <v-card-text>Type: Donation</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
 
-    <v-row class="mb-6" gutters="20">
-      <v-col cols="3" justify="top">
-        <typography>10.7</typography>
-      </v-col>
-      <v-col cols="17">
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Buona Vista</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+          <v-row class="mb-6" gutters="20">
+            <v-col cols="3" justify="top">
+              <typography>Date: 10.7</typography>
+            </v-col>
+            <v-col cols="17">
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Buona Vista</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Pioneer</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Pioneer</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Laboardo Park</v-card-title>
-          <v-card-text>Type: Donation</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Laboardo Park</v-card-title>
+                <v-card-text>Type: Donation</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
 
-    <v-row class="mb-6" gutters="20">
-      <v-col cols="3" justify="top">
-        <typography>10.8</typography>
-      </v-col>
-      <v-col cols="17">
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Buona Vista</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Reschedule</v-btn>
-          </v-card-actions>
-        </v-card>
+          <v-row class="mb-6" gutters="20">
+            <v-col cols="3" justify="top">
+              <typography>Date: 10.8</typography>
+            </v-col>
+            <v-col cols="17">
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Buona Vista</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Reschedule</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Pioneer</v-card-title>
-          <v-card-text>Type: People</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Edit</v-btn>
-          </v-card-actions>
-        </v-card>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Pioneer</v-card-title>
+                <v-card-text>Type: People</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Edit</v-btn>
+                </v-card-actions>
+              </v-card>
 
-        <v-card max-width="344" class="mx-auto">
-          <v-card-title>Address: Laboardo Park</v-card-title>
-          <v-card-text>Type: Donation</v-card-text>
-          <v-card-text>Time: 9:00</v-card-text>
-          <v-card-actions>
-            <v-btn text>Edit</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+              <v-card max-width="344" class="mx-auto">
+                <v-card-title>Address: Laboardo Park</v-card-title>
+                <v-card-text>Type: Donation</v-card-text>
+                <v-card-text>Time: 9:00</v-card-text>
+                <v-card-actions>
+                  <v-btn text>Edit</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -166,7 +172,7 @@ export default {
     return {
       dialog: false,
       tempTime: "9:00",
-      ifChanged: false,
+      ifChanged: false
     };
   },
   methods: {
---
+++ front-end/src/views/driverhome.vue
@@ -0,0 +1,99 @@
+<template>
+    <v-container>
+        <v-layout row class="text-xs-center" >
+            <v-flex xs4 class="grey lighten-4" >
+                <v-container style="position: relative;top: 13%;" class="text-xs-center" >
+                    <v-layout column justify-space-between fill-height wrap>
+                        <v-card
+                                class="mx-auto"
+                                outlined
+                                flat
+                        >
+                            <v-list-item three-line>
+                                <v-list-item-content>
+                                    <div class="overline mb-4">JOB</div>
+                                    <v-list-item-title class="headline mb-1">Job#12</v-list-item-title>
+                                    <v-list-item-subtitle>Ride for 4 person at 5pm from HQ to Bedok</v-list-item-subtitle>
+                                </v-list-item-content>
+
+                                <v-list-item-avatar
+                                        tile
+                                        size="80"
+                                        color="grey"
+                                ></v-list-item-avatar>
+                            </v-list-item>
+
+                            <v-card-actions>
+                                <v-btn text>Accept</v-btn>
+                                <v-btn text>Reject</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        <v-spacer></v-spacer>
+                        <v-card  class="mx-auto"
+                                 outlined>
+                            <v-list-item three-line>
+                                <v-list-item-content>
+                                    <div class="overline mb-4">JOB</div>
+                                    <v-list-item-title class="headline mb-1">Job#13</v-list-item-title>
+                                    <v-list-item-subtitle>Deliver cartons of milk from Jurong to HQ</v-list-item-subtitle>
+                                </v-list-item-content>
+
+                                <v-list-item-avatar
+                                        tile
+                                        size="80"
+                                        color="grey"
+                                ></v-list-item-avatar>
+                            </v-list-item>
+
+                            <v-card-actions>
+                                <v-btn text>Accept</v-btn>
+                                <v-btn text>Reject</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-layout>
+                </v-container>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'driverhome',
+        components: {
+        },
+        data() {
+            return {
+                items:['Staff', 'Driver', 'Admin'],
+                username: '',
+                password: '',
+                role: '' ,
+                output: ''
+            };
+        },
+        methods: {
+            formSubmit(e) {
+                e.preventDefault();
+                let currentObj = this;
+                this.axios.post('https://code-4-good.herokuapp.com/login', {
+                    password: this.password,
+                    username: this.username
+                }).then(function (response) {
+                    // eslint-disable-next-line no-console
+                    console.log(this.role);
+                    currentObj.output = response.data;
+                }).catch(function (error) {
+                    currentObj.output = error;
+                });
+                // eslint-disable-next-line no-console
+                if (this.role == 'Staff'){
+                    this.$router.push('/userform');
+                } else if(this.role == 'Driver'){
+                    this.$router.push('/driver');
+                } else{
+                    this.$router.push('/admin');
+                }
+            }
+        }
+    }
+</script>

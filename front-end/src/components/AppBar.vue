60d0869bfc4cdb0ab956b0c4084e70ff803dc756
johann95ko <johann95ko@hotmail.com>
2019-10-04 12:47:51 +0800

    change nacbar

--- front-end/src/components/AppBar.vue
+++ front-end/src/components/AppBar.vue
@@ -1,44 +1,36 @@
 <template>
-    <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="#fcb69f"
-      dark
-      shrink-on-scroll
-      src="https://picsum.photos/1920/1080?random"
-      scroll-target="#scrolling-techniques-2"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
+    <div>
+        <v-app-bar
+                color="primary"
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <div class="flex-grow-1"></div>
+            
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+            <v-menu
+                    left
+                    bottom
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                </template>
 
-      <div class="flex-grow-1"></div>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-2"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1000px;"></v-container>
-    </v-sheet>
-  </v-card>
+                <v-list>
+                    <v-list-item
+                            v-for="n in 5"
+                            :key="n"
+                            @click="() => {}"
+                    >
+                        <v-list-item-title>Option {{ n }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+    </div>
 </template>
--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,8 +1,25 @@
 <template>
-  <v-container>
-  </v-container>
+  <!-- Material form login -->
+  <form>
+    <p class="h4 text-center mb-4">Sign in</p>
+    <div class="grey-text">
+      <mdb-input label="Your email" icon="envelope" type="email"/>
+      <mdb-input label="Your password" icon="lock" type="password"/>
+    </div>
+    <div class="text-center">
+      <mdb-btn>Login</mdb-btn>
+    </div>
+  </form>
+  <!-- Material form login -->
 </template>
 
 <script>
-
+  import { mdbInput, mdbBtn } from 'mdbvue';
+  export default {
+    name: 'Basic',
+    components: {
+      mdbInput,
+      mdbBtn
+    },
+  }
 </script>
